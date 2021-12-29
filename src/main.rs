ed4faa13f4c7d4b3684c6d2ba327154e743249be
kouki <kouki@localhost>
2021-12-30 07:04:28 +0900

    OK

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod line;
+
 fn main() {
-    println!("Hello, world!");
+    let channel_access_token = std::env::var("CHANNEL_ACCESS_TOKEN").expect("CHANNEL_ACCESS_TOKEN environment variable must be defined");
+    let client = line::Client::new(&channel_access_token);
+    client.broadcast(vec!["charlie", "delta"]).expect("broadcast");
 }
